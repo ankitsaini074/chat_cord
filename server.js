4269b91d08945fc58e3349dfeaa0621015f65481
ankitsaini074 <77288569+ankitsaini074@users.noreply.github.com>
2022-07-23 16:01:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ io.on('connection', socket => {
 
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}` ));
